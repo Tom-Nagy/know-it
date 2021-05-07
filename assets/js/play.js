b1df8452283acfa320d2438c36362e5d63e9737e
Tom-Nagy <tomnagy@hotmail.fr>
2021-05-07 11:41:16 +0000

    Add the Game display function as well as close function in play.js when clicking on the corresponding button in play.html

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,5 +1,6 @@
 // ------------------------------------------------------------------ Display of the rules
 // Build following the tutorial video on Youtube made [by Web Dev Simplified](https://www.youtube.com/watch?v=MBaw_6cPmAw).
+// start credit
 
 let openModalButtons = document.querySelectorAll("[data-modal-target]");
 let closeModalButtons = document.querySelectorAll("[data-close-modal]");
@@ -44,6 +45,7 @@ function closeModal(modal) {
     modal.classList.remove("modal-active");
     overlay.classList.remove("overlay-active");
 }
+// End credit
 
 // ------------------------------------------------------------------ Select the subject
 
@@ -52,6 +54,7 @@ let subjectButtons = document.querySelectorAll("[data-subject-button]");
 subjectButtons.forEach(button => {
     button.addEventListener("click", () => {
         let subject = button.id;
+        console.log(subject);
         if (subject === "volcano") {
             setSubject(subject);
         } else if (subject === "jungle"){
@@ -70,15 +73,14 @@ function setSubject (subject) {
 
     let otherSubjects = document.getElementsByClassName("subjects-buttons");
     for ( let subjects of otherSubjects) {
-        if (subjects.classList.contains("selected-subject") & subjects.id !== subject) {
+        if (subjects.classList.contains("selected-subject") && subjects.id !== subject) {
             subjects.classList.remove("selected-subject");
         }
     }
- return subject;
+
 }
 
-
-// ------------------------------------------------------------------ Toggle the level list
+// ---------------------------- Toggle the level list
 // Add event listener to the levels button 
 $("#levels-button").click(toggleLevels);
 
@@ -102,8 +104,60 @@ levelList.forEach(level => {
 
 function setLevel(getLevel) {
     document.getElementById("button-display").innerText = getLevel;
-    return getLevel;
+    let level = document.getElementById("button-display").innerText;
 }
 
-// -------------------------- start the game.
+// ------------------------------------------------------------------ Game 
 
+// Display (start) the Game
+ let startButton = document.getElementById("start-button");
+ let gameOverlay = document.getElementsByClassName("game-overlay")[0];
+
+ startButton.addEventListener("click", function() {
+    let level = document.getElementById("button-display").innerText;
+    let subject = document.getElementsByClassName("selected-subject")[0];
+
+     if (level === "Apprentice" && typeof(subject) !== "undefined") {
+         displayEasyGame(level, subject);
+     } else if ( level === "Set Level" || typeof(subject) === "undefined"){
+         alert(
+             `Oops, it didn't work!!
+             PLease make sure you selected a SUBJECT and set the LEVEL ;)
+             `);
+     }
+ });
+
+function displayEasyGame(level, subject) {
+    let easyGame = document.getElementById("easy-game");
+    easyGame.classList.add("modal-active");
+    gameOverlay.classList.add("overlay-active");
+
+    document.querySelector("[data-game-level]").innerText = level;
+
+    document.querySelector("[data-game-subject]").innerText = subject.id;
+    //let selectedSubject = document.querySelector("[data-game-subject]").innerText  The question will be selected depending on selectedSubject
+}
+
+// Close (Exit) the Game
+
+let closeGameButton = document.querySelector("[data-exit-game]");
+
+closeGameButton.addEventListener("click", function() {
+    let openGame = document.querySelector(closeGameButton.dataset.exitGame);
+    closeGame(openGame);    
+})
+
+function closeGame(openGame) {
+    openGame.classList.remove("modal-active");
+    gameOverlay.classList.remove("overlay-active");
+}
+
+// Move the Avatar (player)
+
+let controlButtons = document.getElementsByClassName("control-buttons");
+
+controlButtons.forEach(button => {
+    button.addEventListener("click", function() {
+        let buttonDirection = document.querySelector(button.dataset.buttonDirection);
+    })
+})
